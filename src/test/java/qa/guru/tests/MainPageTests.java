1f9fed43f8d81357d0f6030c9ed704ae6ca88082
mikhailrezchikov <mrezchikov529@gmail.com>
2023-09-20 21:38:33 +0300

    fix after review

--- src/test/java/qa/guru/tests/MainPageTests.java
+++ src/test/java/qa/guru/tests/MainPageTests.java
@@ -13,21 +13,21 @@ public class MainPageTests extends BaseTest {
 
     @Test
     @DisplayName("В главном меню есть курс по Java+")
-    void inMainMenuShoudBeJavaPlusCourseTest() {
+    void verifyOpeningOfJavaPlusCoursePageFromHeader() {
         mainPage.openPage().openCourseInMainMenu("Курсы Java+");
         javaPlusCoursePage.checkJavaPlusCoursePageTitle();
     }
 
     @Test
     @DisplayName("В главном меню есть курс по Python")
-    void inMainMenuShoudBePythonCourseTest() {
+    void verifyOpeningOfPythonCoursePageFromHeader() {
         mainPage.openPage().openCourseInMainMenu("Курсы Python");
         pythonCoursePage.checkPythonCoursePageTitle();
     }
 
     @Test
     @DisplayName("При открытии главной страницы qa.guru открывается страница курса по Java")
-    void inMainMenuShoudBeJavaCourseTest() {
+    void verifyOpeningOfJavaCoursePageWhenOpeningQaGuruMainPage() {
         mainPage.openPage();
         javaCoursePage.checkJavaCoursePageTitle();
     }
